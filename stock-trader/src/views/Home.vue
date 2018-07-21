82944a33dd7dd21cad0a5bfc4c9f03e4ef7ca28a
krikchaip <krikchai.p@gmail.com>
2018-07-22 00:02:49 +0700

    stock-trader : styling Home component

---
+++ stock-trader/src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <div class="h2">Trade or View your Portfolio</div>
+      <div class="text-muted">You may Save & Load your Data</div>
+      <div class="text-muted">Click on "End Day" to begin a new Day!</div>
+      <hr>
+      <strong>Your Funds: ${{ (10000).toLocaleString() }}</strong>
+    </div>
+  </div>
+</template>
