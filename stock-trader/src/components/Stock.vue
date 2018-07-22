bb997ccf50f3bc472e1d21a0f76f1405521179c2
krikchaip <krikchai.p@gmail.com>
2018-07-23 00:05:38 +0700

    stock-trader : styling Stock component
    [minor changes]
    - made header responsive

---
+++ stock-trader/src/components/Stock.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="col-12 col-md-6 col-lg-4 px-2 pb-3">
+    <div class="card">
+      <div class="card-header bg-success text-white">
+        BMW
+        <small>[ Price: 100 ]</small>
+      </div>
+      <div class="card-body">
+        <div class="form-row">
+          <input
+            class="form-control form-control-sm col-6 mr-auto"
+            type="number"
+            placeholder="Quantity"
+          />
+          <button class="btn btn-sm btn-success">Buy</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- stock-trader/src/components/TheHeader.vue
+++ stock-trader/src/components/TheHeader.vue
@@ -1,34 +1,39 @@
 <template>
-  <nav class="navbar navbar-light bg-light">
+  <nav class="navbar navbar-expand-md navbar-light bg-light">
     <a class="navbar-brand" draggable="false" href="#">Stock Trader</a>
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" draggable="false" href="#">Portfolio</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" draggable="false" href="#">Stocks</a>
-      </li>
-    </ul>
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
-        <a class="nav-link" draggable="false" href="#">End Day</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a
-          class="nav-link dropdown-toggle"
-          draggable="false"
-          href="#">
-          Save & Load
-        </a>
-        <div class="dropdown-menu show">
-          <a class="dropdown-item" draggable="false" href="#">Save Data</a>
-          <a class="dropdown-item" draggable="false" href="#">Load Data</a>
-        </div>
-      </li>
-      <li class="nav-item navbar-text">
-        <strong>Funds: ${{ (10000).toLocaleString() }}</strong>
-      </li>
-    </ul>
+    <button class="navbar-toggler">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse show">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" draggable="false" href="#">Portfolio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" draggable="false" href="#">Stocks</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" draggable="false" href="#">End Day</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            draggable="false"
+            href="#">
+            Save & Load
+          </a>
+          <div class="dropdown-menu show">
+            <a class="dropdown-item" draggable="false" href="#">Save Data</a>
+            <a class="dropdown-item" draggable="false" href="#">Load Data</a>
+          </div>
+        </li>
+        <li class="nav-item navbar-text">
+          <strong>Funds: ${{ (10000).toLocaleString() }}</strong>
+        </li>
+      </ul>
+    </div>
   </nav>
 </template>
 
