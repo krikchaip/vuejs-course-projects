c0232d0d7c257dc93ccc481ef022a4e0534f28cb
krikchaip <krikchai.p@gmail.com>
2018-07-31 21:20:44 +0700

    stock-trader : home page

--- stock-trader/src/views/Home.vue
+++ stock-trader/src/views/Home.vue
@@ -1,4 +1,11 @@
 <template>
+  <div class="card p-4">
+    <h1 class="h2">Trade or View your Portfolio</h1>
+    <small>You may Save & Load your Data</small>
+    <small>Click on "End Day" to begin a new Day!</small>
+    <hr/>
+    <strong>Your Funds: ${{ $store.getters.funds }}</strong>
+  </div>
 </template>
 
 <script>
---
+++ stock-trader/tests/unit/Home.spec.js
@@ -0,0 +1,15 @@
+import { shallowMount } from '@vue/test-utils'
+import Home from '@/views/Home'
+
+it('display funds from vuex getter', () => {
+  const getters = { funds: '100,000' }
+  const wrapper = shallowMount(Home, {
+    mocks: { $store: { getters } }
+  })
+
+  expect(wrapper.find('strong').text()).toBe('Your Funds: $100,000')
+
+  getters.funds = '99,999'
+  wrapper.vm.$forceUpdate()
+  expect(wrapper.find('strong').text()).toBe('Your Funds: $99,999')
+})
