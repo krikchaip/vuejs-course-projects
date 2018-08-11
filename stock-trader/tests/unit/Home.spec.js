4d80823a2ad80bd15d7ef5ca3b568beac19b72db
krikchaip <krikchai.p@gmail.com>
2018-08-11 17:56:33 +0700

    stock-trader : Home test refactoring

--- stock-trader/tests/unit/Home.spec.js
+++ stock-trader/tests/unit/Home.spec.js
@@ -1,16 +1,16 @@
 import { shallowMount } from '@vue/test-utils'
 import Home from '@/views/Home'
 
-// * NOT BAD - what happen when you don't put funds in <strong> tag?
-it('display funds from vuex getter', () => {
-  const getters = { funds: '100,000' }
-  const wrapper = shallowMount(Home, {
-    mocks: { $store: { getters } }
+describe('funds getter is set', () => {
+  const $store = {
+    getters: { funds: '100,000' }
+  }
+
+  it('should display correct funds with messages', () => {
+    const wrapper = shallowMount(Home, { mocks: { $store } })
+    expect(wrapper.html()).toMatchSnapshot()
+
+    wrapper.vm.$store.getters.funds = '9,000'
+    expect(wrapper.html()).toMatchSnapshot()
   })
-
-  expect(wrapper.find('strong').text()).toBe('Your Funds: $100,000')
-
-  getters.funds = '99,999'
-  wrapper.vm.$forceUpdate()
-  expect(wrapper.find('strong').text()).toBe('Your Funds: $99,999')
 })
