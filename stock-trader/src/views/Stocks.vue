0ef10083323f8106f17f8d346097ab06e1bade9a
krikchaip <krikchai.p@gmail.com>
2018-08-13 13:10:11 +0700

    stock-trader : stocks test refactored

--- stock-trader/src/views/Stocks.vue
+++ stock-trader/src/views/Stocks.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row">
     <div
-      v-for="(data, idx) of getStocksData()"
+      v-for="(data, idx) of stocksData"
       :key="idx"
       class="col-12 col-md-6 mb-3">
       <Stock v-bind="data"/>
@@ -15,13 +15,21 @@
   export default {
     name: 'Stocks',
     components: { Stock },
+    data: () => ({
+      stocksData: null
+    }),
+    async created() {
+      this.stocksData = await this.fetchStocksData()
+    },
     methods: {
-      getStocksData: () => [
-        { name: 'BMW', price: 100 },
-        { name: 'Google', price: 200 },
-        { name: 'Apple', price: 250 },
-        { name: 'Twitter', price: 50 }
-      ]
+      async fetchStocksData() {
+        return [
+          { name: 'BMW', price: 100 },
+          { name: 'Google', price: 200 },
+          { name: 'Apple', price: 250 },
+          { name: 'Twitter', price: 50 }
+        ]
+      }
     }
   }
 </script>
--- stock-trader/tests/unit/Stocks.spec.js
+++ stock-trader/tests/unit/Stocks.spec.js
@@ -1,40 +1,51 @@
 import { shallowMount } from '@vue/test-utils'
 import Stocks from '@/views/Stocks'
 
-describe('getStocksData', () => {
-  // * NOT BAD - but testing for returned results also, concluded a function
-  it('should be a method', () => {
+describe('fetchStocksData', () => {
+  it('should resolve array of objects with name and price', async () => {
     const wrapper = shallowMount(Stocks)
-    expect(wrapper.vm.getStocksData).toBeInstanceOf(Function)
-  })
+    const data = await wrapper.vm.fetchStocksData()
 
-  // * NOT BAD - but relying on implementation
-  it('return array of objects with name and price', () => {
-    const wrapper = shallowMount(Stocks)
-    const StocksData =
-      { name: expect.any(String), price: expect.any(Number) }
-
-    expect(wrapper.vm.getStocksData())
-      .toEqual(expect.arrayContaining([
-        expect.objectContaining(StocksData)
-      ]))
-  })
-
-  // ! "data" depends on other tests
-  it('renders Stock as many as the data', () => {
-    const wrapper = shallowMount(Stocks)
-    const Stock = wrapper.findAll({ name: 'Stock' })
-    const data = wrapper.vm.getStocksData()
-
-    expect(Stock.length).toBe(data.length)
+    expect(data).toEqual(expect.arrayContaining([
+      expect.objectContaining({
+        name: expect.any(String),
+        price: expect.any(Number)
+      })
+    ]))
   })
 })
 
-// * NOT BAD - you shouldn't care what to pass. just expect the result
-describe('Stock', () => {
-  it('passes name and price from data as props', () => {
-    const Stock = shallowMount(Stocks).find({ name: 'Stock' })
-    expect(Stock.props().name).toBeDefined()
-    expect(Stock.props().price).toBeDefined()
+describe('given stocksData initially unset', () => {
+  const stocksData = null
+  const wrapper = shallowMount(Stocks, {
+    data: () => ({ stocksData })
+  })
+
+  it('should initialize when mounted', () => {
+    expect(wrapper.vm.stocksData).toEqual(expect.arrayContaining([
+      expect.objectContaining({
+        name: expect.any(String),
+        price: expect.any(Number)
+      })
+    ]))
+  })
+})
+
+describe('given stocks data prepared', () => {
+  const stocksData = [
+    { name: 'BMW', price: 100 },
+    { name: 'Google', price: 200 },
+    { name: 'Apple', price: 250 },
+    { name: 'Twitter', price: 50 }
+  ]
+  const wrapper = shallowMount(Stocks)
+
+  wrapper.setData({ stocksData })
+
+  it('should render all corresponding data', () => {
+    const Stock = wrapper.findAll({ name: 'Stock' })
+    const StockProps = Stock.wrappers.map(s => s.props())
+
+    expect(StockProps).toMatchObject(stocksData)
   })
 })
