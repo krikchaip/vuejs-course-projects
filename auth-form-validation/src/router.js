148579f6ee0f1acbd5747497c2d1e8f13a678812
krikchaip <krikchai.p@gmail.com>
2018-08-16 12:14:01 +0700

    auth-form-validation : SignUp page

--- auth-form-validation/src/router.js
+++ auth-form-validation/src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/views/Home.vue'
+
+import Home from '@/views/Home'
+import SignUp from '@/views/SignUp'
 
 Vue.use(Router)
 
@@ -10,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignUp
     }
   ]
 })
---
+++ auth-form-validation/src/views/SignUp.vue
@@ -0,0 +1,43 @@
+<template>
+  <form class="form" @submit.prevent="onSubmit">
+    <div class="form__group">
+      <label for="email">Mail</label>
+      <input class="form__input" type="email" id="email"/>
+    </div>
+    <div class="form__group">
+      <label for="age">Your Age</label>
+      <input class="form__input" type="number" id="age"/>
+    </div>
+    <div class="form__group">
+      <label for="password">Password</label>
+      <input class="form__input" type="password" id="password"/>
+    </div>
+    <div class="form__group">
+      <label for="confirm-password">Confirm Password</label>
+      <input class="form__input" type="password" id="confirm-password"/>
+    </div>
+    <div class="form__group">
+      <label for="country">Country</label>
+      <select class="form__input" id="country">
+        <option>USA</option>
+        <option>India</option>
+        <option>UK</option>
+        <option>Germany</option>
+      </select>
+    </div>
+    <div class="form__group">
+      <input class="form__input" type="checkbox" id="accept"/>
+      <label for="accept">Accept Terms of Use</label>
+    </div>
+    <button class="btn">Submit</button>
+  </form>
+</template>
+
+<script>
+  export default {
+    methods: {
+      onSubmit() {
+      }
+    }
+  }
+</script>
